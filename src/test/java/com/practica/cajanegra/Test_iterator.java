b977db07c40366b7fbcf99c1a8617ef9e806fae5
“ruben.blancoba” <ruben.blancoba@alumnos.upm.es>
2022-12-20 06:48:24 +0100

    Cambiado test iterator

--- src/test/java/com/practica/cajanegra/Test_iterator.java
+++ src/test/java/com/practica/cajanegra/Test_iterator.java
@@ -1,13 +1,17 @@
 package com.practica.cajanegra;
 
-import com.binarytree.BinaryTree;
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
-import static org.junit.Assert.assertTrue;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import com.binarytree.BinaryTree;
+import com.binarytree.Node;
 
 public class Test_iterator {
 
@@ -15,43 +19,37 @@ public class Test_iterator {
 
     @Test
     public void caso1() {
-        arbol = new BinaryTree<>("6");
-        List<String> resultList = new ArrayList<>();
-        resultList.add("6");
-
-        assertTrue(iteratorComparation(resultList.iterator(), arbol.iterator()));
+        arbol = new BinaryTree<String>("6");
+       
+        List<String> resultList = new ArrayList<String>();
+        
+        Iterator<String> it=arbol.iterator();
+        
+        while(it.hasNext()) {
+        	resultList.add(it.next());
+        }
+        
+        assertEquals(arbol.toList(),resultList);
     }
 
     @Test
     public void caso2() {
-        arbol = new BinaryTree<>("6");
+    	arbol = new BinaryTree<String>("6");
         arbol.insert("7", arbol.getRoot(), true);
         arbol.insert("8", arbol.getRoot(), false);
         arbol.insert("9", arbol.getRoot().getLeftChild(), true);
         arbol.insert("10", arbol.getRoot().getLeftChild(), false);
 
-        List<String> resultList = new ArrayList<>();
-        resultList.add("6");
-        resultList.add("7");
-        resultList.add("9");
-        resultList.add("10");
-        resultList.add("8");
+        List<String> resultList = new ArrayList<String>();
+        
+        Iterator<String> it=arbol.iterator();
+        
+        while(it.hasNext()) {
+        	resultList.add(it.next());
+        }
 
-        assertTrue(iteratorComparation(resultList.iterator(), arbol.iterator()));
+        assertEquals(arbol.toList(),resultList);
     }
 
-    @Test(expected=Exception.class)
-    public void caso3() {
-        arbol = null;
-        arbol.iterator();
-    }
-
-    public static boolean iteratorComparation(Iterator<String> it1, Iterator<String> it2) {
-        boolean sameIterator = true;
-
-        while (sameIterator && it1.hasNext())
-            if (!it2.hasNext() || !it2.next().equals(it1.next()) || (it2.hasNext() && !it1.hasNext())) sameIterator = false;
-
-        return sameIterator;
-    }
+  
 }
